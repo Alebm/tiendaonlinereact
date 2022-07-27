c272afb7e83d4681884d6c5e8be07d022ae87abd
ABM <abedoyamarin@gmail.com>
2022-07-26 19:51:43 -0500

    Contador con boton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-//import logo from './logo.svg';
+
+import { useState } from 'react';
+
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Counter from './components/counter/counter';
 
 function App() {
+
+  const [show, setShow] = useState(true);
+
+  const handleClick = (quantity) => {
+    console.log(`agrego la cantidad de : ${quantity}`);
+  }
+
   return (
     <div className="App">
       <Navbar/>
       <ItemListContainer greeting={'primer item del list'} />
+      <button onClick={() => setShow(!show) }>Mostrar/Ocultar</button>
+      {show === true ? <Counter stock={10} onAdd = {handleClick} /> : null}
     </div>
   );
 }
---
+++ src/components/counter/counter.js
@@ -0,0 +1,37 @@
+import Button from '../button/Button';
+import { useState, useEffect } from 'react'
+
+
+const Counter = ({stock, onAdd}) => {
+
+useEffect(()=>{
+});
+
+    const [count, setCount] = useState(1);
+
+    const increment = () => {
+        if(count < stock){
+            setCount(count + 1)
+        }
+    }
+
+    const decrement = () => {
+        if(count > 1){
+            setCount(count - 1)
+        }
+    }
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <Button click = {decrement} label = {'-'} />
+            <Button click = {increment} label = {'+'} />
+            <Button click = {()=> onAdd(count)} label = {'Agregar al 🛒'} />
+        </div>
+    )
+
+
+
+}
+
+export default Counter;
